9a08c8b6cf840d82aa5e2e32664b5a9259b77209
John Velis <john@johnvelis.com>
2016-11-06 07:12:09 -0500

    initial commit

---
+++ Demo_UWP_SkiRunRater/Models/SkiRun.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_UWP_SkiRunRater
+{
+    public class SkiRun
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Vertical { get; set; }
+
+        public SkiRun()
+        {
+
+        }
+
+        public SkiRun(int id, string Name, int vertical)
+        {
+            this.ID = id;
+            this.Name = Name;
+            this.Vertical = vertical;
+        }
+    }
+}
+
---
+++ Demo_UWP_SkiRunRater/Models/SkiRunManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_UWP_SkiRunRater
+{
+    public static class SkiRunManager
+    {
+        public static List<SkiRun> GetSkiRuns()
+        {
+            List<SkiRun> skiRuns = new List<SkiRun>();
+
+            // initialize the IList of high scores - note: no instantiation for an interface
+            skiRuns.Add(new SkiRun() { ID = 1, Name = "Buck", Vertical = 200 });
+            skiRuns.Add(new SkiRun() { ID = 2, Name = "Buckaroo", Vertical = 325 });
+            skiRuns.Add(new SkiRun() { ID = 3, Name = "Hoot Owl", Vertical = 655 });
+            skiRuns.Add(new SkiRun() { ID = 4, Name = "Shelburg's Chute", Vertical = 1023 });
+
+            return skiRuns;
+        }
+    }
+}
